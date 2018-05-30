5c93b28d2aebcbf72d35077119b957ba91e45b6f
Felipebr26 <fcamparasilva@gmail.com>
2018-05-30 08:55:37 -0300

    finish

--- src/pages/Requisicao.vue
+++ src/pages/Requisicao.vue
@@ -2,43 +2,46 @@
   <q-page padding class="print-hide">
     <q-btn color="primary" icon="print" class="absolute-top-right" @click="print()" label="Imprimir" />
     <q-btn color="warning" icon="clear" class="absolute-top-left" @click="clear()" label="Limpar" />
-    <q-field label="Exmo. Sr(a)" :label-width="4">
+    <q-field label="Tipo de requisição" class="q-mt-xl" :label-width="4">
+      <q-select filter v-model="type_document" :options="list_tabela"/>
+    </q-field>
+    <q-field label="Exmo. Sr(a)" class="q-mt-sm" :label-width="4">
       <q-input v-model="diretor_nome" value='' />
     </q-field>
-    <q-field label="Diretor da Faculdade de" :label-width="4">
+    <q-field label="Diretor da Faculdade de" class="q-mt-sm" :label-width="4">
       <q-search color="secondary" v-model="diretor_curso">
         <q-autocomplete
           :static-data="{field: 'value', list: list_cursos}"
         />
       </q-search>
     </q-field>
-    <q-field label="Protocolo Nº" :label-width="4">
+    <q-field label="Protocolo Nº" class="q-mt-sm" :label-width="4">
       <q-input v-model="protocolo_n" value='' />
     </q-field>
-    <q-field label="Nome" :label-width="4">
+    <q-field label="Nome" class="q-mt-sm" :label-width="4">
       <q-input v-model="aluno_nome" value='' />
     </q-field>
-    <q-field label="RGM" :label-width="4">
+    <q-field label="RGM" class="q-mt-sm" :label-width="4">
       <q-input v-model="rgm" value='' />
     </q-field>
-    <q-field label="aluno(a) matriculado(a) no " :label-width="4">
+    <q-field label="aluno(a) matriculado(a) no " class="q-mt-sm" :label-width="4">
       <q-search color="secondary" v-model="aluno_ano_letivo">
         <q-autocomplete
           :static-data="{field: 'value', list: list_semestre}"
         />
       </q-search>
     </q-field>
-    <q-field label="letivo do curso de" :label-width="4">
+    <q-field label="letivo do curso de" class="q-mt-sm" :label-width="4">
       <q-search color="secondary" v-model="aluno_curso">
         <q-autocomplete
           :static-data="{field: 'value', list: list_cursos}"
         />
       </q-search>
     </q-field>
-    <q-field label="Respeitosamente requer" :label-width="4" :count="1122">
+    <q-field label="Respeitosamente requer" class="q-mt-sm" :label-width="4" :count="1122">
       <q-input v-model="requerimento" value='' type="textarea" />
     </q-field>
-    <q-field label="Recebi um requerimento do(a) Sr.(a)" :label-width="4">
+    <q-field label="Recebi um requerimento do(a) Sr.(a)" class="q-mt-sm" :label-width="4">
       <q-input v-model="recebi_de" value=''/>
     </q-field>
     <q-modal v-model="maximizedModal" maximized :content-css="{padding: '50px'}">
@@ -80,7 +83,7 @@
               <div class="thirt-text">
                 <div class="margin-bottom-10"><span class="text-weight-bold">Recebi um requerimento do(a) Sr.(a) </span>{{recebi_de}}</div>
                 <div class="margin-bottom-10"><span class="text-weight-bold">Protocolo Nº. </span>{{protocolo_n}}</div>
-                <p><span class="q-body-2"><span class="text-weight-bold">ATENÇÃO:</span> O documento requerido deverá ser retirado do guichê de informações no dia 25/05/2018</span></p>
+                <p><span class="q-body-2"><span class="text-weight-bold">ATENÇÃO:</span> O documento requerido deverá ser retirado do guichê de informações daqui 15 dias úteis</span></p>
                 <div class="row items-center">
                   <div class="col text-center">
                     <hr class="signature">
@@ -101,15 +104,18 @@
 <script>
 import semestre from 'assets/list/semestre.json'
 import cursos from 'assets/list/cursos.json'
+import tabela from 'assets/list/tabela.json'
 import month from 'assets/list/month.json'
 
-console.log(cursos)
+console.log(tabela)
 export default {
   data () {
     return {
       list_semestre: semestre,
+      list_tabela: tabela,
       list_cursos: cursos,
       list_month: month,
+      type_document: '',
       diretor_nome: '',
       diretor_curso: '',
       protocolo_n: '',
@@ -139,6 +145,7 @@ export default {
       return type === 1 ? `Dourados, ${date.getDate()} de ${this.list_month[date.getMonth()]} de ${date.getFullYear()}` : `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
     },
     print () {
+      console.log(this.type_document)
       const self = this
       self.maximizedModal = true
       setTimeout(function () {
